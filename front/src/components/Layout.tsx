9c87ee3a1726a62a0b832dcdc598e666cdcff7f5
chourabieure <34860017+chourabieure@users.noreply.github.com>
2022-12-22 16:21:10 +0100

    init + project setup + mooved everything into /src folder

---
+++ front/src/components/Layout.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, ReactNode } from 'react'
+import styles from './Layout.module.css'
+import React, { } from "react";
+
+interface Props {
+    children?: ReactNode
+}
+
+export default function TransitionLayout({ children }: Props) {
+    const [displayChildren, setDisplayChildren] = useState(children)
+    const [transitionStage, setTransitionStage] = useState('fadeOut')
+    useEffect(() => {
+        setTransitionStage('fadeIn')
+    }, [])
+
+    useEffect(() => {
+        if (children !== displayChildren) setTransitionStage('fadeOut')
+    }, [children, setDisplayChildren, displayChildren])
+
+    return (
+        // <div
+        //     onTransitionEnd={() => {
+        //         if (transitionStage === 'fadeOut') {
+        //             console.log('fading out')
+        //             setDisplayChildren(children)
+        //             setTransitionStage('fadeIn')
+        //         }
+        //     }}
+        //     className={`${styles.content} ${styles[transitionStage]} flex flex-col items-center pt-10`}>
+        //     {displayChildren}
+        // </div>
+        <div>
+            {children}
+        </div>
+    )
+}
---
+++ front/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'], // remove unused styles in production
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
