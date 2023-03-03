20eaf1f7eff7857710e2131edbd25a70bbba5af1
mlvynhrl <91595222+mlvynhrl@users.noreply.github.com>
2023-03-03 15:22:53 +0100

    com

--- front/src/components/Sections/Footer.jsx
+++ front/src/components/Sections/Footer.jsx
@@ -138,7 +138,7 @@ const Footer = () => {
     //   </div>
     // </section>
 
-    // Tentative footer Flowbite
+    // New footer
     <section>
       <footer class="p-4 bg-white sm:p-6 dark:bg-gray-900">
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
